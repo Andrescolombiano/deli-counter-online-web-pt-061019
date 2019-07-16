d2d2d5b8fec18cacd9fbc3279242743027b59c7d
Andres Carvajal <jc1993box@hotmail.com>
2019-07-16 16:33:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,34 +18,12 @@ end
 
 def take_a_number (katz_deli, "name_person")
   name_person.push (name_person)
-    puts "Welcome, #{name_person}. You are number #{katz_deli.index(name_person) + 1} in line."
+    puts "Welcome, #{name_person}. You are number #{katz_deli.length} in line."
 end
 
 
 
 
-
-
-katz_deli = []
-
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    current_line = []
-    katz_deli.each_with_index {|customer, index| current_line << "#{index + 1}. #{customer}"}
-    puts "The line is currently: #{current_line.join(" ")}"
-  end
-end
-
-
-
-
-
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
 
 
 
