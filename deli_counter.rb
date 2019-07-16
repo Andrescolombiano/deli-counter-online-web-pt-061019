3d5815df8e0055c811619deebd098ab1354fb627
Andres Carvajal <jc1993box@hotmail.com>
2019-07-16 16:30:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 katz_deli = []
 
 def line (katz_deli)
-  
- 
   if katz_deli. == 0
     puts "The line is currently empty."
   else
-    katz_deli = []
-    output = "The line is currently"
+   customers_line = []
+   katz_deli.each_with_index {|customer, index| customers_line << "#{index + 1}"
+   #{customer}"}
+    puts "The line is currently: #{customers_line.join (" ")}"
   end
 end
 
@@ -25,21 +25,17 @@ end
 
 
 
+
 katz_deli = []
 
 def line(katz_deli)
-  line_array = [ ]
-  num = 0
-
-  if katz_deli.length == 0 
+  if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif katz_deli.length > 0 
-    while num < katz_deli.length
-      line_array << "#{num + 1}. #{katz_deli[num]}"
-      num += 1 
-    end
-  puts "The line is currently: #{line_array.join(" ")}"
-end
+  else
+    current_line = []
+    katz_deli.each_with_index {|customer, index| current_line << "#{index + 1}. #{customer}"}
+    puts "The line is currently: #{current_line.join(" ")}"
+  end
 end
 
 
@@ -47,29 +43,18 @@ end
 
 
 def take_a_number(katz_deli, name)
-  final_index = katz_deli.length + 1
-  if katz_deli.length == 0 
-    katz_deli << "#{name}" 
-    puts "Welcome, #{name}. You are number 1 in line."
-  elsif katz_deli.length > 0 
-    katz_deli.push(name)
-    puts "Welcome, #{name}. You are number #{final_index} in line."
-  end
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
 
 
 
-
-
-
-
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
-    elsif katz_deli.length > 0 
-      puts "Currently serving #{katz_deli[0]}."
-      katz_deli.shift
+  else
+    puts "Currently serving #{katz_deli.shift}."
   end
-end 
+end
