34b3abf31e0bc6824068809c243066716e41e099
Andres Carvajal <jc1993box@hotmail.com>
2019-07-16 16:21:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,10 @@
 katz_deli = []
 
-def take_a_number (katz_deli, "name_person")
-  name_person.push (name_person)
-    puts "Welcome, #{name_person}. You are number #{katz_deli.index(name_person) + 1} in line."
-end
-
-
-def line (customers)
+def line (katz_deli)
+  
  
-  if customers.count == 0
-    "The line is currently empty."
+  if katz_deli. == 0
+    puts "The line is currently empty."
   else
     katz_deli = []
     output = "The line is currently"
@@ -17,31 +12,64 @@ def line (customers)
 end
 
 
-var katzDeli = [];
 
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`;
-}
 
-function nowServing(katzDeliLine){
-  if(katzDeliLine.length == 0){
-    return "There is nobody waiting to be served!";
-  }
-  else{
-    return `Currently serving ${katzDeliLine.shift()}.`;
-  }
-}
 
-function currentLine(katzDeliLine){
-  if(katzDeliLine == 0){
-    return "The line is currently empty.";
-  }
-  else{
-    var numberedLine = [];
-    for(let i = 0; i < katzDeliLine.length; i++){
-      numberedLine.push((i+1) + ". " + katzDeliLine[i]);
-    }
-    return "The line is currently: " + numberedLine.join(", ");
-  }
-}
+
+def take_a_number (katz_deli, "name_person")
+  name_person.push (name_person)
+    puts "Welcome, #{name_person}. You are number #{katz_deli.index(name_person) + 1} in line."
+end
+
+
+
+
+
+katz_deli = []
+
+def line(katz_deli)
+  line_array = [ ]
+  num = 0
+
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.length > 0 
+    while num < katz_deli.length
+      line_array << "#{num + 1}. #{katz_deli[num]}"
+      num += 1 
+    end
+  puts "The line is currently: #{line_array.join(" ")}"
+end
+end
+
+
+
+
+
+def take_a_number(katz_deli, name)
+  final_index = katz_deli.length + 1
+  if katz_deli.length == 0 
+    katz_deli << "#{name}" 
+    puts "Welcome, #{name}. You are number 1 in line."
+  elsif katz_deli.length > 0 
+    katz_deli.push(name)
+    puts "Welcome, #{name}. You are number #{final_index} in line."
+  end
+end
+
+
+
+
+
+
+
+
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+    elsif katz_deli.length > 0 
+      puts "Currently serving #{katz_deli[0]}."
+      katz_deli.shift
+  end
+end 
